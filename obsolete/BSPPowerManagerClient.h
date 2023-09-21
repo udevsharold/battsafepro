2263c0bbe39c47fbe35e92f6cbb372d622e96b5e
udevs <45226508+udevsharold@users.noreply.github.com>
2023-09-21 11:48:38 +0800

    -obsolete

--- obsolete/BSPPowerManagerClient.h
+++
@@ -1,9 +0,0 @@
-#import <RocketBootstrap/rocketbootstrap.h>
-#import <AppSupport/CPDistributedMessagingCenter.h>
-
-@interface BSPPowerManagerClient : NSObject{
-    CPDistributedMessagingCenter * _messagingCenter;
-}
--(instancetype)init;
--(NSDictionary *)updateState:(BOOL)enableCharging;
-@end
